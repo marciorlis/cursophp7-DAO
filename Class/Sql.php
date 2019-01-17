4679be28e87597269e5d3932a94e9afe848372d1
marciorlis <ecumenico@gmail.com>
2019-01-17 20:31:29 -0200

    corrigida a palavra statment para statement nas linhas7 10 13 e 14

--- Class/Sql.php
+++ Class/Sql.php
@@ -1,52 +1,28 @@
-<?php
-
+<?php 
 class Sql extends PDO {
-
 	private $conn;
-
 	public function __construct(){
-
 		$this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "");
-
 	}
-
-	private function setParams($statment, $parameters = array()){
-
+	private function setParams($statement, $parameters = array()){
 		foreach ($parameters as $key => $value) {
-
-			$this->setParam($key, $value);# code...
-
-		}		
-
+			
+			$this->setParam($statement, $key, $value);
+		}
 	}
-
-private function set_Param($statment, $key, $value){
-
-	$statment->bindParam($key, $value);
-
-}
-
+	private function setParam($statement, $key, $value){
+		$statement->bindParam($key, $value);
+	}
 	public function query($rawQuery, $params = array()){
-
 		$stmt = $this->conn->prepare($rawQuery);
-
 		$this->setParams($stmt, $params);
-		
 		$stmt->execute();
-		
 		return $stmt;
-
 	}
-
 	public function select($rawQuery, $params = array()):array
 	{
-
 		$stmt = $this->query($rawQuery, $params);
-
 		return $stmt->fetchAll(PDO::FETCH_ASSOC);
-
 	}
-
 }
-
-?>
+ ?>
