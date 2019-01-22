3b55d74a4e82d891f5ba21bdebaf4a2f89ca8bc2
marciorlis <ecumenico@gmail.com>
2019-01-22 08:56:03 -0200

    metodos de lista

--- Class/Usuario.php
+++ Class/Usuario.php
@@ -61,12 +61,53 @@ class Usuario  {
 
 	}
 
+
+	public static function getList(){
+
+		$sql = new sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+	}
+
+	public static function search($login){
+
+		$sql = new sql();
+
+		return $sql->select("SELECT * FROM tb_usuarios WHERE deslogin like :SEARCH ORDER BY deslogin", array(':SEARCH'=>"%".$login."%"
+			));
+	}
+
+
+public function login($login, $password){
+
+		$sql = new Sql();
+
+		$results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :LOGIN AND dessenha = :PASSWORD", array(":LOGIN"=>$login,
+			":PASSWORD"=>$password
+		));
+
+		if (count($results) > 0) { // opcional if (isset($results[0]))
+
+			$row = $results[0];
+
+			$this->setIdusuario($row['idusuario']);
+			$this->setDeslogin($row['deslogin']);
+			$this->setDessenha($row['dessenha']);
+			$this->setDtcadastro(new DateTime($row['dtcadastro']));
+
+		} else {
+			throw new Exception("Login e/ou Senha invalidos");
+			
+		}
+
+}
+
 	public function __toString(){
 		return json_encode(array(
-			"id"=>$this->getIdusuario(),
-			"deslogin"=>$this->getDeslogin(),
-			"dessenha"=>$this->getDesenha(),
-			"dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
+			"id | "=>$this->getIdusuario(),
+			"deslogin | "=>$this->getDeslogin(),
+			"dessenha | "=>$this->getDesenha(),
+			"dtcadastro | "=>$this->getDtcadastro()->format("d/m/Y H:i:s")
 
 		));
 	}}
--- index.php
+++ index.php
@@ -1,17 +1,36 @@
 <?php
 
 require_once("config.php");
-
-$root = new Usuario();
-
-$root->loadbyId(4);
-
-echo $root;
+// carrega um usuario
 /*
+$root = new Usuario();
+$root->loadbyId(4);
+echo $root;
+*/
+
+// carrega lisa de usuarios buscvando pelo login
+/*
+$search = Usuario::search("jo");
+echo json_encode($search);
+*/
+
+//carrega um usuario usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("jose","12345678");
+echo $usuario;
+
+/*
+echo "<hr>";
+$lista = Usuario::getList();
+echo json_encode($lista);
+*/
+
+
+/*
+
+echo "<hr>";
 $sql = new Sql();
-
 $usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
 echo json_encode($usuarios);
 */
 ?>
